741f904a215190841d5d1d701339ef4fd740ec46
chris <chrisaliang@outlook.com>
2019-04-14 17:20:12 +0800

    first step of UI

--- eban/views.py
+++ eban/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Board
+
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("Hello world!")
+    boards = Board.objects.all()
+    # boards_name = list()
 
+    # for board in boards:
+    #     boards_name.append(board.name)
+
+    # response_html = '<br>'.join(boards_name)
+    return render(request, 'home.html', {'boards': boards})
