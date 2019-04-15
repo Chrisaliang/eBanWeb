b9f8cb78cc3a60e3231a939eec11d4f535f7834d
chris <chrisaliang@outlook.com>
2019-04-15 18:14:57 +0800

    use bootstrap UI

--- eban/admin.py
+++ eban/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Board
+
 
 # Register your models here.
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+
+
+# admin.AdminSite.register(Board, BoardAdmin)
