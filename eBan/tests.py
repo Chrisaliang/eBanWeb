68b98e3ff5b2730034320d9016f18fa451c4d9a2
chris <chrisaliang@outlook.com>
2019-04-17 09:41:10 +0800

    link to back home

--- eBan/tests.py
+++ eBan/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from .views import board_topics
+
+from .models import Board
 
 
 class HomeTests(TestCase):
@@ -9,3 +13,22 @@ class HomeTests(TestCase):
         url = reverse('home')
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
+
+
+class BoardTopicsTest(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django board.')
+
+    def test_board_topics_view_status_success_code(self):
+        url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_board_topics_view_not_found_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_board_topics_url_resolves_board_topics_view(self):
+        view = resolve('/boards/1/')
+        self.assertEquals(view.func, board_topics)
